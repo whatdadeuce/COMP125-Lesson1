8f82c050c293be8425224a7b34b8b57a06f74b26
whatdadeuce <kenneth_bato16@yahoo.com>
2016-05-11 13:58:10 -0400

    Lab 2 Commit

---
+++ Scripts/app.js
@@ -0,0 +1,10 @@
+//Custom javascript
+//Self executing anonymous function
+//IIFE - immediately invoked function expression
+(function () {
+    console.log("App started...");
+    
+    //Variable declaration and initialization
+    var number = Math.floor((Math.random() * 10)+1);
+    consol.log(number);
+} )( );
